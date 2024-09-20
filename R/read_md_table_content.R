0bee9da93c55cf02cb4be10714f214c36a3fefad
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-09-20 14:53:54 -0400

    Add `read_md_table_content` abstraction

---
+++ R/read_md_table_content.R
@@ -0,0 +1,36 @@
+#' @title Read the Content of a Markdown Table
+#'
+#' @details Performs basic parsing of a markdown table and passes it along to 
+#'   [readr::read_delim].
+#'
+#' @inheritParams read_md_table
+#'
+#' @returns A tibble.
+#'
+#' @keywords internal
+#'
+#' @noRd
+read_md_table_content <- function(markdown_table, warn = TRUE, ...) {
+  markdown_table <- stringr::str_split(markdown_table, "\n")[[1]]
+
+  if (warn) warn_md_table(markdown_table)
+
+  # Remove the header separator line (second line)
+  markdown_table <- markdown_table[-2]
+
+  # Remove leading and trailing pipes and trim whitespace
+  markdown_table <- stringr::str_replace_all(
+    markdown_table,
+    "^\\s*\\|\\s*|\\s*\\|\\s*$",
+    ""
+  )
+
+  markdown_tibble <- readr::read_delim(
+    paste(markdown_table, collapse = "\n"),
+    delim = "|",
+    trim_ws = TRUE,
+    ...
+  )
+
+  return(markdown_tibble)
+}
