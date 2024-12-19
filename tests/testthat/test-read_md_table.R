0970f112069631ba6fdb174bf1fd12616673117d
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-12-19 10:31:36 -0500

    Add GP revdep test

--- tests/testthat/test-read_md_table.R
+++ tests/testthat/test-read_md_table.R
@@ -102,3 +102,20 @@ test_that("read_md_table does not warn when warn = FALSE", {
   input_string <- "| H1 | H2 | \n|---|---|\n| R1C1 | R1C2 ||\n| R2C1 | R2C2 |"
   expect_snapshot(read_md_table(input_string, warn = FALSE, show_col_types = FALSE))
 })
+
+
+test_that("read_md_table handles separator line format of Gutenberg Project's mirror table", {
+  # Essentially a reverse dependency check for https://github.com/ropensci/gutenbergr
+  # https://www.gutenberg.org/MIRRORS.ALL
+  md_file <- test_path("testmd", "gutenberg.md")
+  md <- read_md_table(md_file, warn = FALSE, show_col_types = FALSE)
+
+  expected_tibble <- tibble::tribble(
+    ~Name,   ~Age, ~City,         ~Date,
+    "Alice", 30,   "New York",    lubridate::ymd("2021/01/08"),
+    "Bob",   25,   "Los Angeles", lubridate::ymd("2023/07/22"),
+    "Carol", 27,   "Chicago",     lubridate::ymd("2022/11/01")
+  )
+
+  expect_identical(expected_tibble, md)
+})
